9024123c663d5911343af002897d35f630c4bd69
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-01-18 07:31:45 +0000

    3-glam_rock.sql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,11 @@
+-- Calculate the lifespan and rank Glam rock bands by longevity
+SELECT
+    band_name,
+    COALESCE(split, 2022) - formed AS lifespan
+FROM
+    metal_bands
+WHERE
+    style LIKE '%Glam rock%'
+ORDER BY
+    lifespan DESC;
+
