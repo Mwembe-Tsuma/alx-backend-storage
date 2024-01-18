50185e8aa0b08ff2bb737f59c41d66482d4c671b
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-01-18 08:01:06 +0000

    4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,22 +1,7 @@
 -- Create a trigger to decrease item quantity after a new order
-DELIMITER //
-
 CREATE TRIGGER after_insert_order
-AFTER INSERT ON orders FOR EACH ROW
-BEGIN
-    DECLARE ordered_quantity INT;
-
-    -- Get the quantity ordered in the new order
-    SELECT number INTO ordered_quantity
-    FROM orders
-    WHERE item_name = NEW.item_name
-    ORDER BY number DESC
-    LIMIT 1;
-
-    -- Decrease the item quantity in the items table
-    UPDATE items
-    SET quantity = quantity - ordered_quantity
-    WHERE name = NEW.item_name;
-END;
-//
-DELIMITER ;
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
