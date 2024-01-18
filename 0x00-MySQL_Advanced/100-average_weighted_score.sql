c363108c00921a3e3ed0fb7345077c12cdcdb20f
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-01-18 10:31:09 +0000

    100-average_weighted_score.sql

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,12 @@
+-- Create stored procedure ComputeAverageWeightedScoreForUser
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (
+        SELECT COALESCE(SUM(corrections.score * projects.weight) / SUM(projects.weight), 0)
+        FROM corrections
+        INNER JOIN projects ON projects.id = corrections.project_id
+        WHERE corrections.user_id = user_id
+    )
+    WHERE id = user_id;
+END;
