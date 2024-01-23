43a706c78a0db40f39ab9e4cbad2188eda618c40
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-01-23 14:59:46 +0000

    10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,5 +4,4 @@
 
 def update_topics(mongo_collection, name, topics):
     """Update topics of a school document based on the name."""
-    result = mongo_coll.updates({"name": name}, {"$set": {"topics": topics}})
-    return result.modified_count
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
