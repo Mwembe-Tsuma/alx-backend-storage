815ee168664d333db2db296f04943aff8bd7a5f6
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-01-23 16:54:35 +0000

    102-log_stats.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" 12. Log stats """
+
+
+from pymongo import MongoClient
+
+
+def log_stats():
+    """ Print statistics about Nginx logs in MongoDB. """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+
+    total_logs = logs_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    methods = {method: logs_collection.count_documents({"method": method})
+               for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]}
+    print("Methods:")
+    for method, count in methods.items():
+        print(f"\tmethod {method}: {count}")
+
+    sts = logs_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{sts} status check")
+
+    ip_counts = logs_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+
+    print("IPs:")
+    for ip_count in ip_counts:
+        print(f"\t{ip_count['_id']}: {ip_count['count']}")
+
+
+if __name__ == "__main__":
+    log_stats()
