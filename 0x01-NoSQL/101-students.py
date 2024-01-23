3d9095df0ed6d712c1e22735f920e9d129c7755f
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-01-23 16:34:03 +0000

    101-students.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -3,25 +3,12 @@
 sorted by average score
 """
 
-from pymongo import MongoClient
-
 
 def top_students(mongo_collection):
-    """Return all students sorted by average score."""
-    students = mongo_collection.aggregate([
-        {
-            "$project": {
-                "_id": 1,
-                "name": 1,
-                "scores": 1,
-                "averageScore": {
-                    "$avg": "$scores.score"
-                }
-            }
-        },
-        {
-            "$sort": {"averageScore": -1}
-        }
+    """
+    Returns all students sorted by average score.
+    """
+    return mongo_collection.aggregate([
+        {"$project": {"name": 1, "averageScore": {"$avg": "$topics.score"}}},
+        {"$sort": {"averageScore": -1}}
     ])
-
-    return students
