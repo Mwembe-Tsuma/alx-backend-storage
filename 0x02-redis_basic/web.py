618c23a8b4f963ef87f3f8160acc178b57c569bf
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-01-24 12:25:32 +0000

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,36 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 """Main file"""
 
 
-import requests
 import redis
+import requests
 from functools import wraps
 from typing import Callable
+
 
 redis_client = redis.Redis()
 
 
-def count_and_cache(func: Callable) -> Callable:
-    """Implementing an expiring web cache and tracker"""
-    @wraps(func)
+def track_get_page(fn: Callable) -> Callable:
+    """ Implementing an expiring web cache and tracker """
+    @wraps(fn)
     def wrapper(url: str) -> str:
-        count_key = f"count:{url}"
-        cache_key = f"cache:{url}"
+        count_key = f'count:{url}'
+        cache_key = f'cache:{url}'
 
         redis_client.incr(count_key)
+        cached_page = redis_client.get(cache_key)
+        if cached_page:
+            return cached_page.decode('utf-8')
 
-        cached_result = redis_client.get(cache_key)
-        if cached_result:
-            return cached_result.decode('utf-8')
-
-        result = func(url)
-        redis_client.setex(cache_key, 10, result)
-
-        return result
+        response = fn(url)
+        redis_client.setex(cache_key, 10, response)
+        return response
     return wrapper
 
-@count_and_cache
+
+@track_get_page
 def get_page(url: str) -> str:
+    """Makes a http request to a given endpoint"""
     response = requests.get(url)
     return response.text
